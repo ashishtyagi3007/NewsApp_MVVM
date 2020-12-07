12d3bfe1c441e7da5b1703f9d145b27353251f67
Ashish Tyagi <ashisht@dewsolutions.in>
2020-12-07 20:18:54 +0530

    file updates

---
+++ NewsApp_MVVM/Cells/ArticleTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  ArticleTableViewCell.swift
+//  NewsApp_MVVM
+//
+//  Created by Ashish Tyagi on 07/12/20.
+//
+
+import Foundation
+import UIKit
+
+class ArticleTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel! 
+    
+}
